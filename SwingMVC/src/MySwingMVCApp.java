9f8803641d1dee2372f76243d59632af11f813bd
Mohid71 <mohid.sohail14@gmail.com>
2025-10-09 17:22:56 -0400

    MySwingMVCApp.java

--- SwingMVC/src/MySwingMVCApp.java
+++ SwingMVC/src/MySwingMVCApp.java
@@ -1,17 +1,21 @@
 
 public class MySwingMVCApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		  // Assemble all the pieces of the MVC
-		  Model m = new Model("Sylvain", "Saurel");
-		  View v = new View("MVC with SSaurel");
-		  Controller c = new Controller(m, v);
-		  
-		  // create scanner
-		  Scanner scanner = new Scanner();
-		  
-		  c.initController();
-	}
+    public static void main(String[] args) {
+
+        Model model = new Model("John", "Doe");
+        View view = new View("Cash Register");
+        Controller controller = new Controller(model, view);
+
+
+        Scanner scanner = new Scanner();
+        CashRegister cashRegister = new CashRegister();
+
+
+        controller.setScanner(scanner);
+        controller.setCashRegister(cashRegister);
+
+        controller.initController();
+    }
 
 }
